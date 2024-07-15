89f9471958ce6d9a64b44ef381a9cda96c0bce70
Saurabh <saurabh.d2106@gmail.com>
2024-07-15 10:24:53 +0530

    Added example

--- example01/locals.tf
+++ example01/locals.tf
@@ -1,3 +1,72 @@
 locals {
   prefix = "saurabh"
+
+  location = "eastus"
+
+  address_space = ["10.0.0.0/16"]
+
+  tags = {
+    env = "test"
+  }
+
+  subnets = {
+    public_subnet = {
+
+      address_prefixes = ["10.0.1.0/24"]
+
+    }
+
+    private_subnet = {
+
+      address_prefixes = ["10.0.2.0/24"]
+
+    }
+
+  }
+
+  security_group = ["public_sg", "private_sg"]
+
+  sg_rule = {
+    allow_ssh = {
+      security_group = "public_sg"
+
+      priority                   = 1001
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+
+    }
+
+    allow_80 = {
+
+      security_group = "public_sg"
+
+      priority                   = 1001
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+
+    }
+
+  }
+
+  ip_configurations = {
+    internal = {
+      subnet_id = module.subnets["public_subnet"].subnet.id
+
+      private_ip_address_allocation = "Dynamic"
+
+
+    }
+  }
+
 }
+
--- example01/main.tf
+++ example01/main.tf
@@ -1,19 +1,114 @@
 module "rg" {
 
-    source = "../Azure/resource_group"
+  source = "../azure_modules_14jul/resource_group"
 
-    name = "${local.prefix}-rg"
+  name = "${local.prefix}-rg"
 
-    location = "eastus"
+  location = local.location
 
-    tags = {
-        env = "test"
-    }
-  
+  tags = local.tags
+
 }
 
-output "rg_id" {
+module "vnet" {
 
-    value = module.rg.resource_group_id
-  
+  source = "../azure_modules_14jul/azurerm_virtual_networks"
+
+  name = "${local.prefix}-vnet"
+
+  resource_group_name = module.rg.resource_group.name
+
+  location = local.location
+
+  address_space = local.address_space
+
+  tags = local.tags
+
 }
+
+module "subnets" {
+
+  for_each = local.subnets
+
+  source = "../azure_modules_14jul/azurerm_subnets"
+
+  resource_group_name = module.rg.resource_group.name
+
+  name = each.key
+
+  vnet_name = module.vnet.virtual_network.name
+
+  address_prefixes = each.value.address_prefixes
+
+}
+
+module "security_group" {
+
+  for_each = toset(local.security_group)
+
+  source = "../azure_modules_14jul/azurerm_network_security_group"
+
+  resource_group_name = module.rg.resource_group.name
+
+  location = local.location
+
+  name = each.value
+
+}
+
+module "security_group_rule" {
+
+  source = "../azure_modules_14jul/azurerm_network_security_rule"
+
+  for_each = local.sg_rule
+
+  resource_group_name = module.rg.resource_group.name
+
+  name                        = each.key
+  network_security_group_name = each.value.security_group
+
+  priority                   = each.value.priority
+  direction                  = each.value.direction
+  access                     = each.value.access
+  protocol                   = each.value.protocol
+  source_port_range          = each.value.source_port_range
+  destination_port_range     = each.value.destination_port_range
+  source_address_prefix      = each.value.source_address_prefix
+  destination_address_prefix = each.value.destination_address_prefix
+
+}
+
+module "nic" {
+
+    source = "../azure_modules_14jul/azurerm_network_interface"
+
+    resource_group_name = module.rg.resource_group.name
+
+    location = local.location
+
+    name = "${local.prefix}-vnet"
+
+    enable_ip_forwarding = false
+
+    enable_accelerated_networking = false
+
+    ip_configurations = local.ip_configurations
+
+}
+
+module "public_ip" {
+
+  source = "../azure_modules_14jul/azurerm_public_ip"
+
+  resource_group_name = module.rg.resource_group.name
+
+  location = local.location
+
+  name = "${local.prefix}-public-ip"
+
+  sku = "Basic"
+
+  allocation_method = "Dynamic"
+
+}
+
